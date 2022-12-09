4da302855410743457cb4c16b3c329abed244841
amos <15827562947@163.com>
2022-12-09 17:34:38 +0800

    saferun

---
+++ SafeRun.py
@@ -0,0 +1,37 @@
+# Time : 2022/12/9 16:11
+import json
+
+
+import requests
+
+
+def run(method,url ,headers, payload ,proxies , showInfo = 'false'):
+    url = url
+    payload = payload
+    headers = headers
+    proxies = proxies
+    while True:
+        try:
+            resp = getattr(requests,method)(url, headers = headers, proxies = proxies, json = payload)
+            break
+        except Exception as e:
+            print(e)
+
+    if showInfo == 'true':
+        print(json.dumps(resp.json()))
+        #print(json.dumps(resp.json(), indent=4))
+    elif showInfo == 'false':
+        pass
+
+    return resp
+
+if __name__ == '__main__':
+    header = {'authorization': 'Bearer 7dbf24ec-6204-4e60-bd5b-0766b3898c45'}
+    url = 'https://api.hitalentech.com/api/v1/login'
+    payload = {"username": "cindy", "password": "Cindy@123456"}
+    proxies = {
+        'http': 'http://127.0.0.1:4780',
+        'https': 'http://127.0.0.1:4780'
+    }
+
+    resp = run('post',url,payload,header,proxies, 'true')
